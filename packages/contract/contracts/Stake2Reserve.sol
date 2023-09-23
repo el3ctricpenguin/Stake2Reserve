82229dfc3dba0cd501cd67e0cf5b958d344bcb1a
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2023-09-23 01:15:11 -0400

    contract/#1 - getWeekDay

--- packages/contract/contracts/Stake2Reserve.sol
+++ packages/contract/contracts/Stake2Reserve.sol
@@ -11,7 +11,23 @@ contract Stake2Reserve is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    uint256 NY_SUMMER_TIME_2023_01_01 = 1672545600;
+
+    mapping (uint256=>ReservationData) reservations;
+
+    struct ReservationData{
+        string name;
+        uint256 startingTime; // unix time
+        uint256 endingTime; // unix time
+        uint256 guestCount;
+        // courses;
+    }
+
     constructor() ERC721("Stake2Reserve NFT", "S2R"){}
+
+    function reserve () public {
+        mintNFT();
+    }
 
     function mintNFT () public {
         uint256 newItemId = _tokenIds.current();
@@ -21,5 +37,25 @@ contract Stake2Reserve is ERC721URIStorage{
         _setTokenURI(newItemId, tokenURI);
 
         _tokenIds.increment();
+    }
+
+    function burnNFT (uint256 _tokenId) public {
+        _burn(_tokenId);
+    }
+
+    function getWeekDay(uint256 _unixTime) public view returns(uint256){
+        uint256 diffTimeYear = _unixTime - NY_SUMMER_TIME_2023_01_01; // seconds from 2023/01/01 (NY Summertime, Sunday)
+        console.log("diffTimeYear", diffTimeYear);
+        console.log(diffTimeYear/(60*60*24*7));
+        uint256 diffTimeWeek = diffTimeYear%(60*60*24*7);
+        console.log("diffTimeWeek", diffTimeWeek);
+        console.log("week", diffTimeWeek/(60*60*24));
+        uint256 week = uint256(diffTimeWeek/(60*60*24));
+        console.log("week", week);
+        return week;
+    }
+
+    function exists (uint256 _tokenId) public view returns (bool){
+        return _exists(_tokenId);
     }
 }
--- packages/contract/test/Stake2Reserve.js
+++ packages/contract/test/Stake2Reserve.js
@@ -13,9 +13,39 @@ describe("Stake2Reserve", ()=>{
     }
     describe("NFT", ()=>{
         it("should mint a NFT to msg.sender", async ()=>{
-            const {owner, contract} = await loadFixture(deployContract)
+            const {owner, contract} = await loadFixture(deployContract);
             await contract.mintNFT();
             expect(await contract.ownerOf(0)).to.equal(owner.address);
+        });
+        it("should burn a NFT", async ()=>{
+            const {owner, contract} = await loadFixture(deployContract);
+            await contract.mintNFT();
+            await contract.burnNFT(0);
+            expect(await contract.exists(0)).to.equal(false);
+        });
+    });
+    describe("Time", ()=>{
+        it("should return correct week day #1 (Saturday, 6)", async()=>{
+            const {contract} = await loadFixture(deployContract);
+            // in Date.UTC, 0 is January, 11 is December
+            console.log("01/01/2023 (NY, Unix Time): ", new Date(Date.UTC(2023, 1-1, 1, 0+4, 0, 0)).getTime()/1000);
+            console.log("23/09/2023 (NY, Unix Time): ", new Date(Date.UTC(2023, 9-1, 23, 0+4, 45, 40)).getTime()/1000);
+            expect(await contract.getWeekDay(new Date(Date.UTC(2023, 9-1, 23, 0+4, 45, 40)).getTime()/1000)).to.equal(6);
+        });
+        it("should return correct week day #2 (Wendesday, 3)", async()=>{
+            const {contract} = await loadFixture(deployContract);
+            // in Date.UTC, 0 is January, 11 is December
+            expect(await contract.getWeekDay(new Date(Date.UTC(2023, 9-1, 13, 9+4, 15, 1)).getTime()/1000)).to.equal(3);
+        });
+        it("should return correct week day #3 (Wendesday, 0)", async()=>{
+            const {contract} = await loadFixture(deployContract);
+            // in Date.UTC, 0 is January, 11 is December
+            expect(await contract.getWeekDay(new Date(Date.UTC(2024, 5-1, 5, 3+4, 23, 51)).getTime()/1000)).to.equal(0);
+        });
+        it("should return correct week day #4 (Friday, 5)", async()=>{
+            const {contract} = await loadFixture(deployContract);
+            // in Date.UTC, 0 is January, 11 is December
+            expect(await contract.getWeekDay(new Date(Date.UTC(2024, 12-1, 27, 19+4, 2, 1)).getTime()/1000)).to.equal(5);
         });
     });
 });
