98b9a395eb4105cb21a5ebd5d1c9c1de4ce7e182
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2023-09-22 23:24:05 -0400

    contract - edited .gitignore

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -3,5 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  defaultNetwork: 'hardhat'
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat: {
+    }
+  }
 };
