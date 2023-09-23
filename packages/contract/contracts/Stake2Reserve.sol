3f7f49d1110f15748f0a7e25d1c7d4cb8de08300
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2023-09-22 23:22:13 -0400

    contract - added contract and test code

---
+++ packages/contract/contracts/Stake2Reserve.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract Stake2Reserve{
+    constructor(){}
+}
---
+++ packages/contract/test/Stake2Reserve.js
@@ -0,0 +1,5 @@
+const { loadFixture, time } = require('@nomicfoundation/hardhat-network-helpers');
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe("Stake2Reserve", ()=>{});
