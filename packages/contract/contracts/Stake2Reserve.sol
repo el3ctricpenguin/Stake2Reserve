38227ee9c5fecf85ce594755f29a216cb085eef0
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2023-09-23 00:06:02 -0400

    contract/#1 - mintNFT: primitive feature

--- packages/contract/contracts/Stake2Reserve.sol
+++ packages/contract/contracts/Stake2Reserve.sol
@@ -3,6 +3,23 @@ pragma solidity ^0.8.19;
 
 import "hardhat/console.sol";
 
-contract Stake2Reserve{
-    constructor(){}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract Stake2Reserve is ERC721URIStorage{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Stake2Reserve NFT", "S2R"){}
+
+    function mintNFT () public {
+        uint256 newItemId = _tokenIds.current();
+
+        string memory tokenURI = '{"name": "Stake2Reserve NFT", "description": "Penguin", "image: "https://i.imgur.com/T2F51Kn.jpeg"}';
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+    }
 }
--- packages/contract/test/Stake2Reserve.js
+++ packages/contract/test/Stake2Reserve.js
@@ -2,4 +2,20 @@ const { loadFixture, time } = require('@nomicfoundation/hardhat-network-helpers'
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
-describe("Stake2Reserve", ()=>{});
+describe("Stake2Reserve", ()=>{
+    const deployContract = async()=>{
+        const [owner, otherAccount] = await ethers.getSigners();
+        contractFactory = await ethers.getContractFactory("Stake2Reserve");
+        contract = await contractFactory.deploy();
+        await contract.waitForDeployment();
+
+        return {owner, otherAccount, contract};
+    }
+    describe("NFT", ()=>{
+        it("should mint a NFT to msg.sender", async ()=>{
+            const {owner, contract} = await loadFixture(deployContract)
+            await contract.mintNFT();
+            expect(await contract.ownerOf(0)).to.equal(owner.address);
+        });
+    });
+});
