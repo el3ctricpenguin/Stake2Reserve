86064c9ac6fc7290603e8a2374d459a779156523
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2023-09-24 04:25:13 -0400

    Contract/#6_addStake

--- packages/contract/contracts/S2RAave.sol
+++ packages/contract/contracts/S2RAave.sol
@@ -31,12 +31,50 @@ contract S2RAave{
         USDC.approve(AAVE_POOL_ADDRESS, _amount);
         IPool(aavePool).supply(USDC_ADDRESS, _amount, address(this), 0);
         
-        // addStake(_tokenId, _amount);
+        addStake(_tokenId, _amount);
         emit SupplyUSDCToAave(msg.sender, _amount);
     }
     function withdrawUSDCFromAave(uint256 _amount) public {
         IPool(aavePool).withdraw(USDC_ADDRESS, _amount, address(this));
         USDC.transfer(msg.sender, _amount);
         emit WithdrawUSDCFromAave(msg.sender, _amount);
+    }
+
+    function addStake(uint256 _tokenId, uint256 _amount) private {
+        if(tokenIds.length==0){
+            // initial deposit
+            tokenIds.push(_tokenId);
+            stakeAmount[_tokenId] = _amount; // 100%
+            aTokenTotalAmount = _amount;
+        }else{
+            // Update stakeAmount depend on last amount
+            updateCurrentStake();
+
+            // add tokenId's stake
+            tokenIds.push(_tokenId); // add tokenId
+            stakeAmount[_tokenId] = _amount; // add stake
+            aTokenTotalAmount += _amount;
+        }
+    }
+
+    function updateCurrentStake() private {
+        uint256 aTokenCurrentTotalAmount = aUSDC.balanceOf(address(this));
+        for(uint i=0;i<tokenIds.length;i++){
+            stakeAmount[tokenIds[i]] = uint256((stakeAmount[tokenIds[i]]/aTokenTotalAmount)*aTokenCurrentTotalAmount);
+        }
+        aTokenTotalAmount = aTokenCurrentTotalAmount;
+    }
+
+    function getStake(uint256 _tokenId) public view returns(uint){
+        return stakeAmount[_tokenId];
+    }
+
+    function getCurrentStake(uint256 _tokenId) public view returns(uint){
+        uint256 aTokenCurrentTotalAmount = aUSDC.balanceOf(address(this));
+        return uint256((stakeAmount[_tokenId]/aTokenTotalAmount)*aTokenCurrentTotalAmount);
+    }
+
+    function getATokenTotalAmount() public view returns(uint){
+        return aUSDC.balanceOf(address(this));
     }
 }
