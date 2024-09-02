697d3cafd2794cbd8e6ae4dfb0f6e1a552b02d70
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-09-03 03:11:58 +0900

    client: add contract addresses

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -2,7 +2,7 @@ import { getAddress } from "viem";
 
 export const contractAddresses = {
     MockUSDC: getAddress("0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8"),
-    S2R: getAddress("0x82F051e4b8Bf3E50a65C153054bd9b491586f6BB"),
-    S2RNFT: getAddress("0x443BfB6f711C15D59A7e8517E7c812D483d79173"),
-    S2RAave: getAddress("0x8ce38eb07E3782C454Ea523e48AcDcA8c0B29da9"),
+    S2R: getAddress("0x099048870Af4614dD3ed2A98EFdb60E757F4A756"),
+    S2RNFT: getAddress("0xCEd656AC054C91C99235227f4536489549d70625"),
+    S2RAave: getAddress("0x8Be3602c00c552a709c75145a04A6529A0074d31"),
 };
